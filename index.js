d8cba62e06d541f46651552291b9fcabda1b392d
Branson Morgan <bransox@comcast.net>
2022-08-09 16:03:35 -0400

    commit tree 491801dde3a7ef70e22d974522e197aa7cc3cf5d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// Code your solutions in this file 
+let realCards = [];
+let cards = ["Charlie", "Jennifer", "Branson"];
+function writeCards(cards) {
+    for (let a = 0; a < cards.length; a++) {
+        realCards.push(`Thank you, ${cards[a]}, for the wonderful surprise gift!`);
+        debugger; 
+         } 
+    return realCards;
+}
+
+function countDown() {
+    let countDown = 10
+    while (countDown >= 0) {
+    console.log(countDown--);
+    }
+}
